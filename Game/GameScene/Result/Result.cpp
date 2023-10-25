f94308fe9694531e4b29e916922df7480641ac62
aoaomidri <aoaomidori2723@gmail.com>
2023-10-26 04:25:15 +0900

    リザルトのテクスチャの色が真っ黒なのを修正

--- Game/GameScene/Result/Result.cpp
+++ Game/GameScene/Result/Result.cpp
@@ -119,7 +119,7 @@ void Result::Draw3D(const Matrix4x4& viewProjection){
 void Result::Draw2D(const Matrix4x4& viewProjection){
 	Texture2D::TextureDraw(titleTrans_, viewProjection, 0xffffffff, titleResources_.get());
 	Texture2D::TextureDraw(pressTrans_, viewProjection, pressColor, pressResources_.get());
-	Texture2D::TextureDraw(BTrans_, viewProjection, 0x000000ff, BResources_.get());
+	Texture2D::TextureDraw(BTrans_, viewProjection, 0xffffffff, BResources_.get());
 	for (auto& i : dustTrans_) {
 		Texture2D::TextureDraw(i, viewProjection, 0x0000ccaa, particleResources_.get());
 	}
